04395a9332ac816752502b7868ffc2e70edc1de6
goozzik <jakub.gorzelak@gmail.com>
2010-09-18 09:26:29 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleNewsAggregation::Application.config.secret_token = '1d948e02699ee5f14dd61d96df7fcbbe51e653eb4f58607f13bef294fcfd3f2a8172317d40321d068603ddbcb79879a259f1def0480b0cd33574b6da1a467f52'
