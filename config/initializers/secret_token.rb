d7af3ffaffa99d34438d0041927c7c30a962a408
goozzik <jakub.gorzelak@gmail.com>
2010-10-20 15:55:34 +0200

    new rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleNewsAggregation::Application.config.secret_token = '75d3992fddc1843c92136a60450de9e755ed330247fe306a6a856ce88f4ba2f2621983d5cdcb9aebf575becb710c978a4c63c441881ba8580ae1012aacefb5c5'
