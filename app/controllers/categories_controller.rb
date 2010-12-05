6a8748484d7e48b56755c1a7add15b498bd940f5
goozzik <jakub.gorzelak@gmail.com>
2010-12-05 20:44:05 +0100

    added category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,11 @@ class CategoriesController < ApplicationController
     @categories = Category.all
     @feeds = Feed.all(:order => 'updated_at', :limit => 10)
   end
+
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @feed = Feed.new
+    
+  end
 end
