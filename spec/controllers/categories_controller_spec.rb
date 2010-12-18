6a0bb747552762054ec9cd56070539904ed21a00
goozzik <jakub.gorzelak@gmail.com>
2010-12-18 13:07:07 +0100

    rspec tests for category controller

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -14,6 +14,12 @@ describe CategoriesController do
       get :index
       assigns(:categories).should eq([mock_category])
     end
+
+    it "assigns 10 last entries as @entries" do
+      last_entries = Entry.all(:order => 'updated_at', :limit => 10)
+      get :index
+      assigns(:entries).should eq(last_entries)
+    end
   end
 
   describe "GET show" do
@@ -22,106 +28,38 @@ describe CategoriesController do
       get :show, :id => "37"
       assigns(:category).should be(mock_category)
     end
-  end
-
-  describe "GET new" do
-    it "assigns a new category as @category" do
-      Category.stub(:new) { mock_category }
-      get :new
-      assigns(:category).should be(mock_category)
+    
+    it "assigns all categories as @categories" do
+      Category.stub(:find).with("37") { mock_category }
+      get :show, :id => "37"
+      assigns(:categories).should eq(Category.all)
+    end
+    
+    it "assigns a new feed as @feed" do
+      Category.stub(:find).with("37") { mock_category }
+      get :show, :id => "37"
+      assigns(:feed).should be_kind_of(Feed)
     end
   end
 
-  describe "GET edit" do
+  describe "GET last_entries" do
     it "assigns the requested category as @category" do
       Category.stub(:find).with("37") { mock_category }
-      get :edit, :id => "37"
+      get :last_entries, :id => "37"
       assigns(:category).should be(mock_category)
     end
-  end
 
-  describe "POST create" do
-
-    describe "with valid params" do
-      it "assigns a newly created category as @category" do
-        Category.stub(:new).with({'these' => 'params'}) { mock_category(:save => true) }
-        post :create, :category => {'these' => 'params'}
-        assigns(:category).should be(mock_category)
-      end
-
-      it "redirects to the created category" do
-        Category.stub(:new) { mock_category(:save => true) }
-        post :create, :category => {}
-        response.should redirect_to(category_url(mock_category))
-      end
+    it "assigns a new feed as @feed" do
+      Category.stub(:find).with("37") { mock_category }
+      get :last_entries, :id => "37"
+      assigns(:feed).should be_kind_of(Feed)
     end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved category as @category" do
-        Category.stub(:new).with({'these' => 'params'}) { mock_category(:save => false) }
-        post :create, :category => {'these' => 'params'}
-        assigns(:category).should be(mock_category)
-      end
-
-      it "re-renders the 'new' template" do
-        Category.stub(:new) { mock_category(:save => false) }
-        post :create, :category => {}
-        response.should render_template("new")
-      end
-    end
-
-  end
-
-  describe "PUT update" do
-
-    describe "with valid params" do
-      it "updates the requested category" do
-        Category.should_receive(:find).with("37") { mock_category }
-        mock_category.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "37", :category => {'these' => 'params'}
-      end
-
-      it "assigns the requested category as @category" do
-        Category.stub(:find) { mock_category(:update_attributes => true) }
-        put :update, :id => "1"
-        assigns(:category).should be(mock_category)
-      end
-
-      it "redirects to the category" do
-        Category.stub(:find) { mock_category(:update_attributes => true) }
-        put :update, :id => "1"
-        response.should redirect_to(category_url(mock_category))
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the category as @category" do
-        Category.stub(:find) { mock_category(:update_attributes => false) }
-        put :update, :id => "1"
-        assigns(:category).should be(mock_category)
-      end
-
-      it "re-renders the 'edit' template" do
-        Category.stub(:find) { mock_category(:update_attributes => false) }
-        put :update, :id => "1"
-        response.should render_template("edit")
-      end
-    end
-
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested category" do
-      Category.should_receive(:find).with("37") { mock_category }
-      mock_category.should_receive(:destroy)
-      delete :destroy, :id => "37"
-    end
-
-    it "redirects to the categories list" do
-      Category.stub(:find) { mock_category }
-      delete :destroy, :id => "1"
-      response.should redirect_to(categories_url)
+    it "assigns a 25 entries per page as @entries" do
+      Category.stub(:find).with("37") { mock_category }
+      entries = mock_category.entries.all(:limit => 25, :order => 'updated_at DESC') 
+      get :last_entries, :id => "37"
+      assigns(:entries).should eq(entries) 
     end
   end
-
 end
